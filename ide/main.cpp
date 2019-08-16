69cca20abf69ad7c973bcde139b003c0786ed8f5
Piemontez <piemontez@live.com>
2019-08-16 16:06:29 -0300

    start main

--- ide/main.cpp
+++ ide/main.cpp
@@ -1,5 +1,26 @@
-int main(int /*argc*/, char **/*argv*/)
-{
+#include <QApplication>
+#include <QMainWindow>
+#include <QWidget>
 
-    return 0;
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  if (false)
+    setvbuf(stdout, nullptr, _IONBF, 0);
+
+  QLocale::setDefault(QLocale(QLocale::Portuguese, QLocale::Brazil));
+  QApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
+  QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+  QApplication a(argc, argv);
+  
+  a.setQuitOnLastWindowClosed(true);
+  a.setApplicationName(APP_FULL_NAME);
+
+
+  QMainWindow mainWindow;
+  mainWindow.setCentralWidget(new QWidget);
+  mainWindow.show();
+
+  return a.exec();
 }

1dec79dfda22d4725bd36a4324ccd5e750b8f5d9
piemontez <rafaelpiemontez@gmail.com>
2022-04-16 11:50:59 -0300

    add opening, clossing and morphologyEx components

--- src/plugins/opencv/morphology/index.ts
+++ src/plugins/opencv/morphology/index.ts
@@ -1,11 +1,63 @@
-import { CVFIOComponent } from 'renderer/types/component';
+import { CVFComponent, CVFIOComponent } from 'renderer/types/component';
 import { CVFNodeProcessor } from 'renderer/types/node';
 import cv, { Scalar, Point, Mat } from 'opencv-ts';
 import { PropertyType } from 'renderer/types/property';
 import { BorderTypes } from 'opencv-ts/src/core/CoreArray';
 import GCStore from 'renderer/contexts/GCStore';
+import { MorphTypes } from 'opencv-ts/src/ImageProcessing/ImageFiltering';
+import { SourceHandle, TargetHandle } from 'renderer/types/handle';
+import { Position } from 'react-flow-renderer/nocss';
 
 const tabName = 'Morphology';
+
+/**
+ * Erode component and node
+ */
+export class ErodeComponent extends CVFIOComponent {
+  static menu = { tabTitle: tabName, title: 'Erode' };
+  static processor = class ErodeNode extends CVFNodeProcessor {
+    static properties = [
+      { name: 'kernel', type: PropertyType.OneZeroMatrix },
+      { name: 'anchor', type: PropertyType.Point },
+      { name: 'iterations', type: PropertyType.Integer },
+      { name: 'borderType', type: PropertyType.BorderType },
+      { name: 'borderValue', type: PropertyType.Scalar },
+    ];
+    kernel: Mat = cv.getStructuringElement(
+      cv.MORPH_RECT,
+      new cv.Size(3, 3),
+      new cv.Point(-1, -1)
+    );
+    anchor: Point = new cv.Point(-1, -1);
+    iterations: number = 1;
+    borderType: BorderTypes = cv.BORDER_CONSTANT;
+    borderValue: Scalar = cv.morphologyDefaultBorderValue();
+
+    async proccess() {
+      const { inputsAsMat: inputs } = this;
+      if (inputs.length) {
+        this.sources = [];
+        for (const src of inputs) {
+          const out = new cv.Mat(src.rows, src.cols, src.type());
+          GCStore.add(out);
+
+          cv.erode(
+            src,
+            out,
+            this.kernel,
+            this.anchor,
+            this.iterations,
+            this.borderType,
+            this.borderValue
+          );
+
+          this.sources.push(out);
+          this.output(out);
+        }
+      }
+    }
+  };
+}
 
 /**
  * Dilate component and node
@@ -57,11 +109,11 @@ export class DilateComponent extends CVFIOComponent {
 }
 
 /**
- * Erode component and node
+ * Opening component and node
  */
-export class ErodeComponent extends CVFIOComponent {
-  static menu = { tabTitle: tabName, title: 'Erode' };
-  static processor = class ErodeNode extends CVFNodeProcessor {
+export class OpeningComponent extends CVFIOComponent {
+  static menu = { tabTitle: tabName, title: 'Opening' };
+  static processor = class OpeningNode extends CVFNodeProcessor {
     static properties = [
       { name: 'kernel', type: PropertyType.OneZeroMatrix },
       { name: 'anchor', type: PropertyType.Point },
@@ -69,6 +121,7 @@ export class ErodeComponent extends CVFIOComponent {
       { name: 'borderType', type: PropertyType.BorderType },
       { name: 'borderValue', type: PropertyType.Scalar },
     ];
+
     kernel: Mat = cv.getStructuringElement(
       cv.MORPH_RECT,
       new cv.Size(3, 3),
@@ -87,16 +140,121 @@ export class ErodeComponent extends CVFIOComponent {
           const out = new cv.Mat(src.rows, src.cols, src.type());
           GCStore.add(out);
 
-          cv.erode(
+          cv.morphologyEx(
             src,
             out,
+            cv.MORPH_OPEN,
             this.kernel,
             this.anchor,
             this.iterations,
             this.borderType,
             this.borderValue
           );
+          this.sources.push(out);
+          this.output(out);
+        }
+      }
+    }
+  };
+}
 
+/**
+ * Closing component and node
+ */
+export class ClosingComponent extends CVFIOComponent {
+  static menu = { tabTitle: tabName, title: 'Closing' };
+  static processor = class ClosingNode extends CVFNodeProcessor {
+    static properties = [
+      { name: 'kernel', type: PropertyType.OneZeroMatrix },
+      { name: 'anchor', type: PropertyType.Point },
+      { name: 'iterations', type: PropertyType.Integer },
+      { name: 'borderType', type: PropertyType.BorderType },
+      { name: 'borderValue', type: PropertyType.Scalar },
+    ];
+
+    kernel: Mat = cv.getStructuringElement(
+      cv.MORPH_RECT,
+      new cv.Size(3, 3),
+      new cv.Point(-1, -1)
+    );
+    anchor: Point = new cv.Point(-1, -1);
+    iterations: number = 1;
+    borderType: BorderTypes = cv.BORDER_CONSTANT;
+    borderValue: Scalar = cv.morphologyDefaultBorderValue();
+
+    async proccess() {
+      const { inputsAsMat: inputs } = this;
+      if (inputs.length) {
+        this.sources = [];
+        for (const src of inputs) {
+          const out = new cv.Mat(src.rows, src.cols, src.type());
+          GCStore.add(out);
+
+          cv.morphologyEx(
+            src,
+            out,
+            cv.MORPH_CLOSE,
+            this.kernel,
+            this.anchor,
+            this.iterations,
+            this.borderType,
+            this.borderValue
+          );
+          this.sources.push(out);
+          this.output(out);
+        }
+      }
+    }
+  };
+}
+
+/**
+ * MorphologyEx component and node
+ */
+export class MorphologyExComponent extends CVFComponent {
+  static menu = { tabTitle: tabName, title: 'Morphology Ex' };
+
+  targets: TargetHandle[] = [
+    { title: 'src', position: Position.Left },
+    { title: 'kernel', position: Position.Left },
+  ];
+  sources: SourceHandle[] = [{ title: 'out', position: Position.Right }];
+
+  static processor = class MorphologyExNode extends CVFNodeProcessor {
+    static properties = [
+      { name: 'op', type: PropertyType.OneZeroMatrix },
+      { name: 'anchor', type: PropertyType.Point },
+      { name: 'iterations', type: PropertyType.Integer },
+      { name: 'borderType', type: PropertyType.BorderType },
+      { name: 'borderValue', type: PropertyType.Scalar },
+    ];
+
+    op: MorphTypes = cv.MORPH_OPEN;
+    anchor: Point = new cv.Point(-1, -1);
+    iterations: number = 1;
+    borderType: BorderTypes = cv.BORDER_CONSTANT;
+    borderValue: Scalar = cv.morphologyDefaultBorderValue();
+
+    async proccess() {
+      const { inputsAsMat: inputs } = this;
+      this.sources = [];
+      if (inputs.length === 2) {
+        const [src, kernel] = inputs;
+
+        if (src && kernel) {
+          const out = new cv.Mat(src.rows, src.cols, src.type());
+          GCStore.add(out);
+
+          cv.morphologyEx(
+            src,
+            out,
+            this.op,
+            kernel,
+            this.anchor,
+            this.iterations,
+            this.borderType,
+            this.borderValue
+          );
           this.sources.push(out);
           this.output(out);
         }

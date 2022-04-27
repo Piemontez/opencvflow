849e0568dce65656aa4598366694453d1ebba5c0
piemontez <rafaelpiemontez@gmail.com>
2022-04-26 23:04:11 -0300

    add pyr down and up

--- src/plugins/opencv/others/index.ts
+++ src/plugins/opencv/others/index.ts
@@ -1,6 +1,6 @@
 import { CVFIOComponent } from 'renderer/types/component';
 import { CVFNodeProcessor } from 'renderer/types/node';
-import cv, { Point, Mat, BackgroundSubtractorMOG2 } from 'opencv-ts';
+import cv, { Point, Mat, BackgroundSubtractorMOG2, Size } from 'opencv-ts';
 import { PropertyType } from 'renderer/types/property';
 import { BorderTypes } from 'opencv-ts/src/core/CoreArray';
 import {
@@ -209,6 +209,70 @@ export class GrabCutComponent extends CVFIOComponent {
       this.mask = undefined;
       this.bgdModel = undefined;
       this.fgdModel = undefined;
+    }
+  };
+}
+
+/**
+ * Pyr Down Transform component and node
+ */
+export class PyrDownComponent extends CVFIOComponent {
+  static menu = { tabTitle: tabName, title: 'PyrDown' };
+  static processor = class PyrDownNode extends CVFNodeProcessor {
+    static properties = [
+      { name: 'dstsize', type: PropertyType.Size },
+      { name: 'borderType', type: PropertyType.DistanceTransformMasks },
+    ];
+
+    dstsize: Size = new cv.Size(0, 0);
+    borderType: BorderTypes = cv.BORDER_DEFAULT;
+
+    async proccess() {
+      const { inputsAsMat: inputs } = this;
+      if (inputs.length) {
+        this.sources = [];
+        for (const src of inputs) {
+          const out = new cv.Mat(src.rows, src.cols, src.type());
+          GCStore.add(out);
+
+          cv.pyrDown(src, out, this.dstsize, this.borderType);
+
+          this.sources.push(out);
+          this.output(out);
+        }
+      }
+    }
+  };
+}
+
+/**
+ * Pyr Up Transform component and node
+ */
+export class PyrUpComponent extends CVFIOComponent {
+  static menu = { tabTitle: tabName, title: 'PyrUp' };
+  static processor = class PyrUpNode extends CVFNodeProcessor {
+    static properties = [
+      { name: 'dstsize', type: PropertyType.Size },
+      { name: 'borderType', type: PropertyType.DistanceTransformMasks },
+    ];
+
+    dstsize: Size = new cv.Size(0, 0);
+    borderType: BorderTypes = cv.BORDER_DEFAULT;
+
+    async proccess() {
+      const { inputsAsMat: inputs } = this;
+      if (inputs.length) {
+        this.sources = [];
+        for (const src of inputs) {
+          const out = new cv.Mat(src.rows, src.cols, src.type());
+          GCStore.add(out);
+
+          cv.pyrUp(src, out, this.dstsize, this.borderType);
+
+          this.sources.push(out);
+          this.output(out);
+        }
+      }
     }
   };
 }

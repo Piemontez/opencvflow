dda7b03e6d85f247fcacdfa3fa25bb0b5f9e6e72
piemontez <rafaelpiemontez@gmail.com>
2023-01-19 12:27:31 -0300

    fix kernel type

--- src/plugins/opencv/inputs/index.tsx
+++ src/plugins/opencv/inputs/index.tsx
@@ -5,7 +5,7 @@ import { PropertyType } from 'renderer/types/property';
 import GCStore from 'renderer/contexts/GCStore';
 import { Position } from 'react-flow-renderer/nocss';
 import { SourceHandle } from 'renderer/types/handle';
-import { VideoSizes } from 'renderer/config/sizes';
+import { NodeSizes, VideoSizes } from 'renderer/config/sizes';
 import { DataTypes } from 'opencv-ts/src/core/HalInterface';
 import messages from '../messages';
 
@@ -219,19 +219,63 @@ export class CVKernelComponent extends CVFOutputComponent {
 
     dataType: DataTypes = cv.CV_8U;
     kernel: Mat = new cv.Mat(3, 3, this.dataType, new cv.Scalar(0));
+    out: Mat = new cv.Mat(3, 3, this.dataType, new cv.Scalar(0));
+
+    async start() {
+      this.makeOutput();
+    }
 
     async propertyChange(name: string, value: any): Promise<void> {
       if (name === 'dataType') {
-        const newKernel = new cv.Mat(3, 3, this.dataType, new cv.Scalar(0));
-        GCStore.add(newKernel, -100);
+        this.changeDataType(value as number);
+      } else if (name === 'kernel') {
+        this.makeOutput();
+      }
+    }
 
-        this.kernel.convertTo(newKernel, value);
-        this.kernel = newKernel;
+    changeDataType(value: number) {
+      GCStore.add(this.kernel, -100);
 
-        if (value > cv.CV_8U) {
-          this.properties[1].type = PropertyType.DoubleMatrix;
-        } else {
-          this.properties[1].type = PropertyType.OneZeroMatrix;
+      const newKernel = new cv.Mat(3, 3, this.dataType, new cv.Scalar(0));
+
+      this.kernel.convertTo(newKernel, value);
+      this.kernel = newKernel;
+
+      const isUType = [cv.CV_8UC1, cv.CV_8UC2, cv.CV_8UC3, cv.CV_8UC4].includes(
+        value
+      );
+
+      if (isUType) {
+        this.properties[1].type = PropertyType.OneZeroMatrix;
+      } else {
+        this.properties[1].type = PropertyType.DoubleMatrix;
+      }
+    }
+
+    makeOutput() {
+      GCStore.add(this.out, -100);
+
+      const min = Math.min(this.kernel.rows, this.kernel.cols);
+      const scale = NodeSizes.defaultHeight / min;
+      const cols = this.kernel.cols * scale;
+      const rows = this.kernel.rows * scale;
+
+      this.out = new cv.Mat(rows, cols, this.kernel.type());
+      const dsize = new cv.Size(cols, rows);
+      cv.resize(this.kernel, this.out, dsize, 0, 0, cv.INTER_AREA);
+
+      const isUType = [cv.CV_8UC1, cv.CV_8UC2, cv.CV_8UC3, cv.CV_8UC4].includes(
+        this.kernel.type() as number
+      );
+
+      if (isUType) {
+        const channels = this.out.channels();
+        for (let j = 0; j < cols; j++) {
+          for (let k = 0; k < rows; k++) {
+            for (let i = 0; i < channels; i++) {
+              this.out.charPtr(k, j)[i] = this.out.charPtr(k, j)[i] ? 255 : 0;
+            }
+          }
         }
       }
     }
@@ -239,16 +283,7 @@ export class CVKernelComponent extends CVFOutputComponent {
     async proccess() {
       this.sources = [this.kernel];
 
-      const out = new cv.Mat();
-      const dsize = new cv.Size(
-        Math.max(150, this.kernel.cols),
-        Math.max(150, this.kernel.rows)
-      );
-      cv.resize(this.kernel, out, dsize, 0, 0, cv.INTER_AREA);
-
-      GCStore.add(out);
-
-      this.output(out);
+      this.output(this.out);
     }
   };
 }

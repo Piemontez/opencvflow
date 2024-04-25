8b9e5dcd762dcfd3a87c09209b0e60f1b3587c1c
piemontez <rafaelpiemontez@gmail.com>
2024-04-24 23:46:11 -0300

    revisão estilo e nova logo

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
+//import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     //
     react(),
-    dts(),
+    //dts(),
   ],
   build: {
     rollupOptions: {

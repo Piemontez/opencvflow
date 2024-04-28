ddf121b20256acd092e1b245127c081a224325c5
piemontez <rafaelpiemontez@gmail.com>
2024-04-28 15:42:23 -0300

    boot loader

--- bin/build.sh
+++ bin/build.sh
@@ -4,8 +4,10 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
-# Instala dependências
+# Dependências
+echo 'Instalando dependências'
 yarn
 
-# Inicia a aplicação
+# Build
+echo 'Buildando aplicação'
 yarn build
